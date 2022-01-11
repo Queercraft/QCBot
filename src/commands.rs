72555d71088317c30afb7456598bef63b2b02816
diademiemi <emilia@diademiemi.me>
2022-01-11 03:22:38 +0100

    Allow negative values in temp

--- src/commands.rs
+++ src/commands.rs
@@ -4,7 +4,7 @@ pub fn commands(command: String, content: String) -> String {
         // If the command is temp
         "temp" => {
             // Get all the numbers (and periods) in the message
-            let degrees = content.chars().filter(|c| c.is_digit(10) || c == &'.').collect::<String>();
+            let degrees = content.chars().filter(|c| c.is_digit(10) || c == &'.' || c == &'-').collect::<String>();
             // Search for a C or F
             let format = content.to_lowercase().chars().find(|c| c == &'c' || c == &'f');
             // If a format was found
@@ -13,22 +13,24 @@ pub fn commands(command: String, content: String) -> String {
                 if !degrees.is_empty() {
                     if f == 'f' {
                         // Check if the number is finite
-                        let fahrenheit: f32 = degrees.parse().unwrap();
-                        if fahrenheit.is_finite() {
-                            // Perform the calculation
-                            let result: f32 = (fahrenheit - 32.0) / 1.8;
-                            // Set the output string
-                            output = format!("{} in Fahrenheit is {} in Celsius.", fahrenheit, (result * 100.00).round() / 100.0);
-                        } else {
-                            output = "I dunno lol".to_string();
+                        if let Some(fahrenheit) = degrees.parse::<f32>().ok() {
+                            if fahrenheit.is_finite() {
+                                // Perform the calculation
+                                let result: f32 = (fahrenheit - 32.0) / 1.8;
+                                // Set the output string
+                                output = format!("{} in Fahrenheit is {} in Celsius.", fahrenheit, (result * 100.00).round() / 100.0);
+                            } else {
+                                output = "I dunno lol".to_string();
+                            }
                         }
                     } else if f == 'c' {
-                        let celsius: f32 = degrees.parse().unwrap();
-                        if celsius.is_finite() {
-                            let result: f32 = (celsius * 1.8) + 32.0;
-                            output = format!("{} in Celsius is {} in Fahrenheit.", celsius, (result * 100.00).round() / 100.0);
-                        } else {
-                            output = "I dunno lol".to_string();
+                        if let Some(celsius) = degrees.parse::<f32>().ok() {
+                            if celsius.is_finite() {
+                                let result: f32 = (celsius * 1.8) + 32.0;
+                                output = format!("{} in Celsius is {} in Fahrenheit.", celsius, (result * 100.00).round() / 100.0);
+                            } else {
+                                output = "I dunno lol".to_string();
+                            }
                         }
                     }
                 }
