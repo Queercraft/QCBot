4460dd5d933158e850a080e20216c97a52059622
diademiemi <emilia@diademiemi.me>
2022-01-13 03:46:52 +0100

    Add comments to timezone code

--- src/commands.rs
+++ src/commands.rs
@@ -140,32 +140,44 @@ pub fn commands(command: String, content: String) -> String {
             }
         }
         "timezone" => {
-            // Get all characters matching 0-9, : and A/P and M
+            // Get all characters matching 0-9 and :
             let mut time_str = content.split(' ').take(1).next().unwrap_or_default().to_uppercase()
             .chars().filter(|c| c.is_digit(10) || c == &':').collect::<String>();
+            // Attempt to get the AM/PM string
             let meridiem_str = content.split(' ').take(1).next().unwrap_or_default().to_uppercase()
             .chars().filter(|c| c == &'A' || c == &'P' || c == &'M').collect::<String>();
+            // Check if the string specifies minutes, if not, assume 00
             if !time_str.contains(":") {
                 time_str.push_str(":00");
             }
+            // Merge the time string and AM/PM (meridiem)
             time_str.push_str(&meridiem_str);
+            // Attempt to get the first timezone
             let tz_str = content.split(' ').skip(1).next().unwrap_or_default().
             chars().filter(|c| c == &'/' || c == &'_' || c.is_ascii_alphabetic()).collect::<String>();
+            // Attempt to get a second timezone
             let to_tz_str = content.split(' ').skip(2).next().unwrap_or_default().
             chars().filter(|c| c == &'/' || c == &'_' || c.is_ascii_alphabetic()).collect::<String>();
+            // Check if a timezone was found
             if !tz_str.is_empty() {
+                // Check if timezone 1 can be parsed
                 if let Ok(tz) = Tz::from_str(&tz_str) {
+                    // Check if the time is in 12 hour format, if it is, convert it to 24 hours before proceeding
                     if let Ok(ntime) = NaiveTime::parse_from_str(&time_str, "%-I:%M%p") {
                         time_str = ntime.format("%H:%M").to_string();
                     }
+                    // Attempt to parse the time as 24 hours
                     if let Ok(ntime) = NaiveTime::parse_from_str(&time_str, "%H:%M") {
-                        let tz_now: DateTime<Tz> = Utc::today().with_timezone(&tz).and_time(ntime).unwrap();
+                        // Get the time in the specified time zone with the current date in that timezone
+                        let tz_today: DateTime<Tz> = Utc::today().with_timezone(&tz).and_time(ntime).unwrap();
+                        // If a second timezone can be parsed, convert the two timezones and link to a time.is comparison table
                         if let Ok(to_tz) = Tz::from_str(&to_tz_str) {
                             output = format!("{} {} today in {} is {}, more at https://time.is/compare/{}_in_{}/{}",
-                            ntime.format("%H:%M").to_string(), tz.name(), to_tz.name(), tz_now.with_timezone(&to_tz).format("%H:%M"), ntime.format("%H%M").to_string(), tz.name().split('/').last().unwrap(), to_tz.name().split('/').last().unwrap());
+                            ntime.format("%H:%M").to_string(), tz.name(), to_tz.name(), tz_today.with_timezone(&to_tz).format("%H:%M"), ntime.format("%H%M").to_string(), tz.name().split('/').last().unwrap(), to_tz.name().split('/').last().unwrap());
+                        // If only one timezone was specified, use Discord's Unix Timestamp embed and link to a time.is table using an unspecified timezone
                         } else {
                             output = format!("{} {} today in your local timezone is <t:{}>, more at https://time.is/compare/{}_in_{}",
-                            ntime.format("%H:%M").to_string(), tz.name(),tz_now.format("%s").to_string() , ntime.format("%H%M").to_string(), tz.name().split('/').last().unwrap());
+                            ntime.format("%H:%M").to_string(), tz.name(),tz_today.format("%s").to_string() , ntime.format("%H%M").to_string(), tz.name().split('/').last().unwrap());
                         }
                     }
                 }
